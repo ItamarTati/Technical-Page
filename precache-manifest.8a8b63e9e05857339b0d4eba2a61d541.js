7d095999e3e1a6c069a61c9bd1bf263243e159c2
Itamar Tati <itamar.tati1994@gmail.com>
2019-06-17 11:13:42 +0100

    Updates

---
+++ precache-manifest.8a8b63e9e05857339b0d4eba2a61d541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1965e02c74320386897ab11b9862cb",
+    "url": "/Technical-Page/index.html"
+  },
+  {
+    "revision": "fe605449de92b607f8bc",
+    "url": "/Technical-Page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Technical-Page/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fe605449de92b607f8bc",
+    "url": "/Technical-Page/static/js/main.cbcd77d6.chunk.js"
+  },
+  {
+    "revision": "39362659adab70eb3afa",
+    "url": "/Technical-Page/static/js/runtime~main.c543fae5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Technical-Page/static/media/logo.5d5d9eef.svg"
+  }
+]);
